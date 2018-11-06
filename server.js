dda7b8f76f0fec1b339abc8dc59904904e316fa4
ckopecky <ckopecky@github.com>
2018-11-06 01:08:54 -0800

    update

--- server.js
+++ server.js
@@ -7,16 +7,17 @@ const port = process.env.PORT || 5000;
 const cors = require("cors");
 const helmet = require("helmet");
 const mongoose = require("mongoose");
-const database = "ux1lecturesdb";
+const database = "heroku_lrgcbcfr";
 const user = process.env.USERS; //when you use USER, the computer reads it as user of PC - so it read ckopecky and not what's in .env file. Don't user USER. 
 const password = process.env.PW;
 
 const lectureController = require("./lectureController");
 const mongoOptions =  { useNewUrlParser: true } 
 //database connection
-mongoose.connect(`mongodb://${user}:${password}@ds153093.mlab.com:53093/heroku_lrgcbcfr`
+mongoose.connect(`mongodb://${user}:${password}@ds153093.mlab.com:53093/${database}`
     , mongoOptions)
     .then((mongoose)=> {
+        console.log(user, password)
         console.log(`Connected to ${database} on mongoDb`);
     })
     .catch(err => {
